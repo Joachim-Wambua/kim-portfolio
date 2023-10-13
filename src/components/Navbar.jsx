506d606f6974f5efdfbfe0bc80af1c8cd329c60d
Joachim-Wambua <kimwambua96@gmail.com>
2023-10-13 22:44:36 +0200

    finalised Navbar section and made it responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,8 +26,9 @@ const Navbar = () => {
           >
             <img src={logo} alt='logo' className='w-p h-9 object-contain' />
             <p
-              className='text-white text-[18px] font-bold cursor-pointer'
-            >Joachim <span className='sm:block hidden'>| Full Stack Engineer</span></p>
+              className='text-white text-[18px] font-bold cursor-pointer flex'
+            >Joachim&nbsp;<span className='sm:block hidden'>| Full Stack Engineer</span>
+            </p>
           </Link>
 
           <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -59,6 +60,32 @@ const Navbar = () => {
                 setToggle(!toggle)
               }}
             />
+
+            {/* Menu */}
+            <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`} >
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {
+                navLinks.map((link) => (
+                  <li
+                    key={link.id}
+                    className={`${active === link.title
+                        ? "text-white"
+                        : "text-secondary"
+                      } hover:text-white text-[18px]
+                      font-medium cursor-pointer text-[16px]
+                      `}
+                    onClick={() => {
+                        setToggle(!toggle);
+                        setActive(link.title);
+                      }
+                    }
+                  >
+                    <a href={`#${link.id}`}>{link.title}</a>
+                  </li>
+                ))
+              }
+            </ul>
+            </div>
           </div>
 
         </div>
